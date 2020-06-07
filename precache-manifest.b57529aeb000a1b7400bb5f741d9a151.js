40d2368b76243ddd8326599c888c712fe287adc9
yuliya hlukhatarenka <yuliya_hlukhatarenka@epam.com>
2020-06-07 19:07:38 +0300

    Updates

---
+++ precache-manifest.b57529aeb000a1b7400bb5f741d9a151.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7c10e5fa5cb0db34d3d94cec17b82f",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "574b92a5767b11101548",
+    "url": "/weather_app/static/css/main.f24029d3.chunk.css"
+  },
+  {
+    "revision": "3abcd5dc7104d72e7c79",
+    "url": "/weather_app/static/js/2.d47336cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather_app/static/js/2.d47336cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "574b92a5767b11101548",
+    "url": "/weather_app/static/js/main.e6d27a6c.chunk.js"
+  },
+  {
+    "revision": "f556d0f7adfc1e4bf237",
+    "url": "/weather_app/static/js/runtime-main.b014981e.js"
+  },
+  {
+    "revision": "de9c99687ac2d51a08e4539d2aa92d16",
+    "url": "/weather_app/static/media/background.de9c9968.jpg"
+  }
+]);
